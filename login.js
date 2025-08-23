8935eeb31d518ad700075c9cf38802707e7ec3fb
TechExp24 <phillysburgersoxford@gmail.com>
2025-08-23 13:26:28 +0100

    Update login.js

--- login.js
+++ login.js
@@ -21,6 +21,7 @@ const users = [
   { email: "bantu.maruthi990@gmail.com", password: "1999", name: "Bantu", business: ["phillys-cowley", "phillys-stclements", "ricks-diner", "stclaire-valentine"] },
   { email: "sbenbakhti@gmail.com", password: "1234", name: "Sami", business: ["phillys-cowley", "phillys-stclements", "ricks-diner", "stclaire-valentine"] },
   { email: "mourad2001@yahoo.com", password: "1972", name: "Mourad", business: ["phillys-cowley", "phillys-stclements"] },
+  { email: "honorio", password: "1980", name: "Mourad", business: ["phillys-stclements"] },
 ];
 
 // ✅ Quotes
