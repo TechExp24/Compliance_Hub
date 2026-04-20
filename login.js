abaef0ebcde028d13fc7e6700b61a49aea730da2
TechExp24 <phillysburgersoxford@gmail.com>
2026-04-20 22:47:16 +0530

    Add new users to the login.js file

--- login.js
+++ login.js
@@ -5,6 +5,8 @@ const webhookURL = "https://script.google.com/macros/s/AKfycbzdB6oglQvB5MNBy_5Od
 // ✅ All users with names
 const users = [
   { email: "India.chambers11@gmail.com", password: "2004", name: "India", business: ["ricks-diner", "stclaire-valentine"] },
+  { email: "abrarsuhaib98@gmail.com", password: "1998", name: "Suhaib", business: ["ricks-diner", "stclaire-valentine"] },
+  { email: "selmanewafi3@gmail.com", password: "2007", name: "Wafi", business: ["ricks-diner", "stclaire-valentine"] },
   { email: "alexa.xandra.mae@gmail.com", password: "2003", name: "Alexandra", business: ["stclaire-valentine"] },
   { email: "cerysljenkins@gmail.com", password: "2003", name: "Cerys", business: ["stclaire-valentine"] },
   { email: "mdaullah86@gmail.com", password: "1986", name: "Ahsan", business: ["stclaire-valentine"] },
